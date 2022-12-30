f2ed41d177489b4b60cf9e896c42a422a0fad685
CDavidSV <88672259+CDavidSV@users.noreply.github.com>
2022-12-30 00:34:54 -0800

    Implemented Fireworks

--- src/js/firework.js
+++ src/js/firework.js
@@ -1,103 +1,212 @@
 // Document variables.
 const canvas = document.querySelector('.canvas');
 const context = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 // Variables.
 const mainColor = '#0F0F0F';
+const fireworksArr = [];
+const particlesArr = [];
+let fireworkLoop;
+let loop = false;
 
 context.fillStyle = mainColor;
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
 
 // Events.
 window.addEventListener('resize', resizeCanvas);
 
-class Particle {
-    gravity = 2;
-
-    constructor(posX, posY, size, vel, acc, angle, color) {
-        this.size = size;
+class Firework {
+    constructor(posX, posY, color, radius, dx, dy, gravity) {
         this.posX = posX;
         this.posY = posY;
-        this.vel = vel;
-        this.acc = acc;
+        this.radius = radius;
+        this.gravity = gravity;
+        this.dx = dx;
+        this.dy = dy;
         this.color = color;
-        this.angle = - angle * (Math.PI / 180);
-    }
-
-    drawParticle() {
-        context.beginPath();
-        context.fillStyle = "white";
-        context.arc(this.posX, this.posY, this.size / 2, 0, Math.PI * 2, true);
-        context.closePath();
-        context.fill();
-    }
-
-    addVel() {
-        const vx = this.vel * Math.cos(this.angle);
-        const vy = this.vel * Math.sin(this.angle);
-
-        this.posX += vx;
-        this.posY += vy;
     }
 
     update() {
-        context.fillStyle = mainColor;
-        context.fillRect(0,0,canvas.width, canvas.height);
-        this.addVel();
-        this.drawParticle();
+        this.posY += this.dy;
+        this.posX += this.dx;
+        this.dy += this.gravity;
     }
- 
+
+    drawFirework() {
+        context.fillStyle = this.color;
+        context.beginPath();
+        context.arc(this.posX, this.posY, this.radius, 0, Math.PI * 2, true);
+        context.closePath();
+        context.fill();
+    }
 }
 
-class Firework {
-    constructor(particles, time, color) {
-        this.particles = particles;
-        this.time = time;
+class Particle {
+    constructor(posX, posY, color, radius, angle, speed) {
+        this.speed = speed;
+        this.posX = posX;
+        this.posY = posY;
+        this.radius = radius;
+        this.angle = angle;
+        this.gravity = 0.1;
+        this.friction = 0.95;
+        this.dx = this.speed * Math.cos(angle * Math.PI / 180);
+        this.dy = this.speed * Math.sin(angle * Math.PI / 180);
         this.color = color;
     }
 
-    shootFirework() {
+    update() {
+        this.posY += this.dy;
+        this.posX += this.dx;
 
-    }
-}
+        this.dy *= this.friction;
+        this.dx *= this.friction;
+        this.gravity *= this.friction;
 
-class FireworkShow {
-    loop = false;
-    fireworkLoop;
-    
-    constructor() {
+        this.dy += this.gravity;
+    }
 
-    }
-    
-    generateRandomFirework() {
-    
-    }
-    
-    startLoop() {
-        if (this.loop === true) return;
-        this.loop = true;
-        fireworkLoop = setInterval(generateRandomFirework, getRand(500, 1500));
-    }
-    
-    endLoop() {
-        if (this.loop === false) return;
-        this.loop = false;
-        clearInterval(fireworkLoops);
+    drawParticle() {
+        context.fillStyle = this.color;
+        context.beginPath();
+        context.arc(this.posX, this.posY, this.radius, 0, Math.PI * 2, true);
+        context.closePath();
+        context.fill();
     }
 }
 
 // Functions.
+
+// Resizes the canvas when user resizes their browser window.
 function resizeCanvas() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 }
 
-function getRand(min, max) {
+function shootFirework(dirX, dirY) {
+    const rocketSize = 2;
+    const initPosX = getRand(canvas.width / 4, canvas.width / 2 + canvas.width / 4);
+    const initPosY = canvas.height - rocketSize;
+    const gravity = 0.15;
+    const maxHeight = (canvas.height - dirY) * 2;
+    dirX -= initPosX;
+    const angle = Math.atan2(maxHeight, dirX);
+    
+    const v0 = Math.sqrt((gravity * dirX * 2) / (Math.sin(2 * angle)));
+    const dY = -v0 * Math.sin(angle);
+    const dX = v0 * Math.cos(angle);
 
+    const rocket = new Firework(initPosX, initPosY, randomColor(), rocketSize, dX, dY, gravity);
+    fireworksArr.push({rocket: rocket, maxHeight: canvas.height - maxHeight / 2, deleteTimeout: false});
 }
 
-const particle = new Particle(200, 800, 20, 50, 0, 90, "white");
-setInterval(() => {
-    particle.update();
-}, 100);
+function randomFirework() {
+    const rocketSize = 2;
+    const initPosX = getRand(50, canvas.width - 50);
+    const initPosY = canvas.height - rocketSize;
+    const gravity = 0.15;
+    const maxHeight = getRand(400, canvas.height - 50);
+    
+    const dY = -Math.sqrt(2 * gravity * maxHeight);
+
+    const rocket = new Firework(initPosX, initPosY, randomColor(), rocketSize, 0, dY, gravity);
+    fireworksArr.push({rocket: rocket, maxHeight: canvas.height - maxHeight, deleteTimeout: false});
+}
+
+function startLoop() {
+    if (loop === true) return;
+    loop = true;
+    fireworkLoop = setInterval(() => {
+        randomFirework();
+    }, getRand(300, 500));
+}
+
+function endLoop() {
+    if (loop === false) return;
+    loop = false;
+    clearInterval(fireworkLoop);
+}
+
+function getRand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function randomColor() {
+    const colorArr = [
+        '#ff1100', // Red.
+        '#ff9100', // Orange.
+        '#fff700', // Yellow.
+        '#40ff00', // Green.
+        '#00b3ff', // Light Blue.
+        '#0022ff', // Blue.
+        '#bd00b6', // Purple.
+        '#ff59f1', // Pink.
+        '#c2c2c2', // Silver.
+        '#ffffff', // White
+    ];
+
+    return colorArr[getRand(0, colorArr.length - 1)];
+}
+
+function explodeFirework(xPos, yPos, color) {
+    const particles = getRand(25, 40);
+
+    for (let particle = 0; particle < particles; particle++) {
+        const angle = particle / particles * 360;
+        const sparkParticle = new Particle(xPos, yPos, color, 1.5, angle, getRand(5, 10));
+        particlesArr.push(sparkParticle);
+    }
+}
+
+function fireworksBurst(fireworks) {
+    if (fireworks > 200) {
+        fireworks = 200;
+    } else if (fireworks < 1) {
+        fireworks = 1;
+    }
+
+    for (let i = 0; i < Math.round(fireworks); i++) {
+        randomFirework();
+    }
+}
+
+// Main animation function
+function animate() {
+    // Slowly fade out the drawn rockets over time (Each iteration of the animation loop).
+    context.globalCompositeOperation = 'source-over';
+    context.fillStyle = `rgba(15, 15, 15, 0.1)`;
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.globalCompositeOperation = "lighter";
+
+    for (let firework = 0; firework < fireworksArr.length; firework++) {
+        fireworksArr[firework].rocket.update();
+        fireworksArr[firework].rocket.drawFirework();
+
+        if (fireworksArr[firework].rocket.posY <= fireworksArr[firework].maxHeight) {
+            explodeFirework(fireworksArr[firework].rocket.posX, fireworksArr[firework].rocket.posY, fireworksArr[firework].rocket.color);
+            fireworksArr.splice(firework, 1);
+        }
+    }
+
+    for (let particle = 0; particle < particlesArr.length; particle++) {
+        particlesArr[particle].update();
+        particlesArr[particle].drawParticle();
+
+        const newRadius = particlesArr[particle].radius -= 0.022;
+        if (newRadius <= 0) {
+            particlesArr[particle].radius = 0;
+            particlesArr.splice(particle, 1);
+        } else {
+            particlesArr[particle].radius = newRadius;
+        }
+    }
+
+    // Update the rockets position over time.
+    requestAnimationFrame(animate);
+}
+
+
+animate();
--- src/js/main.js
+++ src/js/main.js
@@ -4,10 +4,21 @@ const h = document.querySelector(".hours");
 const m = document.querySelector(".minutes");
 const s = document.querySelector(".seconds");
 const info = document.querySelector(".info");
+const infoCont = document.querySelector(".information-container");
+const title = document.querySelector(".title");
+const countdownContainer = document.querySelector(".countdown-container");
 
 // Variables.
+let countdown;
+let isNewYear = false;
+
+// Events.
+window.addEventListener('click', (e) => {shootFirework(e.clientX, e.clientY)});
+document.addEventListener('visibilitychange', handleVisibilityChange);
 
 // Functions.
+
+// Returns the corresponding day of the wekk given a number from 0 to 6.
 function getDayString(day) {
     if (day > 6 || day < 0) return;
 
@@ -24,6 +35,7 @@ function getDayString(day) {
     return weekDays[day];
 }
 
+// Returns the corresponding month to a 0 indexed number.
 function getMonthString(month) {
     if (month > 11 || month < 0) return
 
@@ -45,6 +57,7 @@ function getMonthString(month) {
     return months[month];
 }
 
+// Animates the seconds counter (Called when 10 seconds or less are remaining in the countdown).
 function animateSeconds() {
     s.style = `transform: scale(1.3)`;
 
@@ -53,10 +66,20 @@ function animateSeconds() {
     }, 400);
 }
 
+// FUnction called every second to calculate the time remaining till the end timestamp.
 function calculateTimeRemaining(startTime, endTime) {
     const timeRemaining = (endTime - startTime) / 1000;
-    if (timeRemaining <= 10) {
+    
+    if (Math.floor(timeRemaining) <= 10) {
         animateSeconds();
+    }
+
+    if (Math.floor(timeRemaining) <= 0) {
+        setNewyearText();
+        fireworksBurst(20);
+        isNewYear = true;
+        setTimeout(startLoop, 1000); // Happy New Year.
+        clearInterval(countdown);
     }
 
     d.textContent = Math.floor(timeRemaining / 86400);
@@ -65,17 +88,44 @@ function calculateTimeRemaining(startTime, endTime) {
     s.textContent = Math.floor(timeRemaining % 60);
 }
 
+function handleVisibilityChange() {
+    if (document.visibilityState == "visible" && isNewYear) {
+        startLoop();
+    } else {
+        endLoop();
+    }
+}
+
+function setNewyearText() {
+    infoCont.style.background = "none";
+    title.innerHTML = "";
+    info.innerHTML = "";
+    countdownContainer.innerHTML = "<div class=\"new-year\"><p>Hapy New Year!</p></div>";
+    setTimeout(() => {document.querySelector(".new-year").style = 'transform: scale(1);'},100);
+}
+
 // Main.
 (function () {
     const currentTime = new Date();
     const newYear = new Date(currentTime.getFullYear() + 1, 0, 1);
+    const dayAfter = new Date(currentTime.getFullYear() + 1, 0, 2);
 
     const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone.split("/")[1].replace("_", " ");
     info.textContent = `Time until ${getDayString(newYear.getDay())}, ${getMonthString(newYear.getMonth())} ${newYear.getDate()}, ${newYear.getFullYear()} (${timeZone} Time)`;
+    
+    if (currentTime >= newYear && currentTime < dayAfter) {
+        isNewYear = true;
+        setNewyearText();
+        startLoop();
+    }
 
-    calculateTimeRemaining(currentTime, newYear);
-    const countdown = setInterval(() => {
-        const start = new Date().getTime();
-        calculateTimeRemaining(start, newYear);
-    }, 1000);
+    if (currentTime <= newYear) {
+        calculateTimeRemaining(currentTime, newYear);
+        countdown = setInterval(() => {
+            const start = new Date().getTime();
+            calculateTimeRemaining(start, newYear);
+        }, 1000);
+    } else {
+        startLoop();
+    }
 })();
